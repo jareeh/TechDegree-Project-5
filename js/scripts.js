fcb94bd64b428c09a4812c1bc3524b53341667f1
Jeff Reeh <65681066+jareeh@users.noreply.github.com>
2020-09-25 10:02:50 -0500

    add full functionality of search, needs refactoring

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 const gallery = document.getElementById('gallery');
 const search = document.querySelector('.search-container');
 let objects = [];
+let filteredObjects = [];
+
 
 fetch('https://randomuser.me/api/?results=12')
     .then(res => res.json())
@@ -10,6 +12,7 @@ fetch('https://randomuser.me/api/?results=12')
             objects.push(res.results[i]);
         }
         console.log(objects);
+        filteredObjects = objects.map(x => x);
     })
     .catch(error => console.log('there was an error retrieving the users', error))
 
@@ -39,6 +42,7 @@ function generateGallery(results){
 
 
 function generateModal(i){
+    //remove modal if it's already there to prevent a ton of hidden HTML elements
     if(document.querySelector('.modal-container')){
         document.querySelector('.modal-container').parentNode.removeChild(document.querySelector('.modal-container'));
     }
@@ -49,14 +53,14 @@ function generateModal(i){
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
-                <img class="modal-img" src="${objects[i].picture.medium}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${objects[i].name.first} ${objects[i].name.last}</h3>
-                <p class="modal-text">${objects[i].email}</p>
-                <p class="modal-text cap">${objects[i].location.city}</p>
+                <img class="modal-img" src="${filteredObjects[i].picture.medium}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${filteredObjects[i].name.first} ${filteredObjects[i].name.last}</h3>
+                <p class="modal-text">${filteredObjects[i].email}</p>
+                <p class="modal-text cap">${filteredObjects[i].location.city}</p>
                 <hr>
-                <p class="modal-text">${objects[i].cell}</p>
-                <p class="modal-text">${objects[i].location.street.number} ${objects[i].location.street.name}, ${objects[i].location.city}, ${objects[i].location.state} ${objects[i].location.postcode}</p>
-                <p class="modal-text">Birthday: ${objects[i].dob.date.substr(5, 2)}/${objects[i].dob.date.substr(8, 2)}/${objects[i].dob.date.substr(0, 4)}</p>
+                <p class="modal-text">${filteredObjects[i].cell}</p>
+                <p class="modal-text">${filteredObjects[i].location.street.number} ${filteredObjects[i].location.street.name}, ${filteredObjects[i].location.city}, ${filteredObjects[i].location.state} ${filteredObjects[i].location.postcode}</p>
+                <p class="modal-text">Birthday: ${filteredObjects[i].dob.date.substr(5, 2)}/${filteredObjects[i].dob.date.substr(8, 2)}/${filteredObjects[i].dob.date.substr(0, 4)}</p>
             </div>
         </div>
         <div class="modal-btn-container">
@@ -74,7 +78,7 @@ function generateModal(i){
     if(index === 0){
         prev.style.display = 'none';
     }
-    if(index === objects.length - 1){
+    if(index === filteredObjects.length - 1){
         next.style.display = 'none';
     }
 
@@ -87,7 +91,7 @@ function generateModal(i){
     //event listener for next button
     next.addEventListener('click', () => {
         document.querySelector('.modal-container').hidden = true;
-        if(!(index >= objects.length - 1)){
+        if(!(index >= filteredObjects.length - 1)){
             generateModal(index+1);
         }
 
@@ -115,7 +119,7 @@ function generateSearch(){
 generateSearch();
 
 function runSearch(){
-    let filteredObjects = [];
+    filteredObjects = [];
     let names = [];
     const searchValue = document.getElementById('search-input').value;
     for (let i = 0;i<objects.length;i++){
@@ -142,10 +146,9 @@ search.addEventListener('keyup', (e) => {
 gallery.addEventListener('click', (e) => {
     const card = e.target.closest('.card');
     const name = card.querySelector('.card-name').textContent;
-    console.log(name.split(' ')[0])
 
-    for (i=0;i<objects.length;i++){
-        if(name.split(' ')[0] === objects[i].name.first && name.split(' ')[1] === objects[i].name.last){
+    for (i=0;i<filteredObjects.length;i++){
+        if(name.split(' ')[0] === filteredObjects[i].name.first && name.split(' ')[1] === filteredObjects[i].name.last){
             generateModal(i);
         }
     }
